6152c6ad87aed62e6626945ae09c7cbd8551e1d1
Maryam1994-cor <mhakami@marymount.edu>
2026-07-15 11:18:38 +0500

    Update create_database.sql

--- database/create_database.sql
+++ database/create_database.sql
@@ -3,7 +3,6 @@
 -- Database design and scripting concepts are based on Chapters 23 and 24
 -- of the assigned course textbook.
 
-
 CREATE DATABASE IF NOT EXISTS language_learning_db
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
